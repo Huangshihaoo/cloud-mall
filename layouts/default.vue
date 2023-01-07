28e2d0065438f34996358e5e37583f8a968e4618
Huangshihao <huangshihao1122@163.com>
2023-01-07 18:04:26 +0800

    布局

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="layout">
+        <header class="header container">
+            <div class="logo">网易云音乐*商城</div>
+        </header>
+        <slot></slot>
+        <footer class="footer container"></footer>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    height: 80px;
+    line-height: 80px;
+    background-color: #fff;
+
+    .logo {
+        display: inline-block;
+        width: 300px;
+        height: 40px;
+        margin-top: 20px;
+        background-image: url(@/assets/images/modules.png);
+        background-repeat: no-repeat;
+        background-position: 0px -63px;
+        font-size: 0px;
+    }
+}
+</style>
